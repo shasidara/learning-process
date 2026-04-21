40a4bdf32dd4d179ce0b83370b2437b77b040237
Your Name <you@example.com>
2026-04-21 13:41:06 +0530

    Dynamic signup API created

--- src/app.js
+++ src/app.js
@@ -3,14 +3,10 @@ const { connectDB } = require("./config/database");
 const User = require("./models/user");
 
 const app = express();
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Virat",
-        lastName: "Kohli",
-        email: "virat@gmail.com",
-        password: "Vit@123",
-    });
+    const user = new User(req.body);
 
     try {
         await user.save();
